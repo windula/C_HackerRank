4acb4f72c581db2fe18e5cdbcc897ffddbfc3f5f
Windula Dissanayake <charithwinwindula@gmail.com>
2022-04-21 12:59:55 +0530

    created first file

---
+++ 01 Hello world in C/Hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char s[100];
+    printf("Enter the text\n");
+    gets(s);
+    printf("Hello, World!\n"); 
+    puts(s);
+    return 0;
+}
+
